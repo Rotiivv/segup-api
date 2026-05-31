7bede0e51bfdc1e0bb5f13f9447dcceb35a4bf6d
rotiv <vitormendesesilva1@gmail.com>
2026-05-31 05:13:33 -0300

    feat: adds valitadion on CreateRegistrationDTO

--- api/src/main/java/backend/segup/api/domain/registration/DTOs/CreateRegistrationDTO.java
+++ api/src/main/java/backend/segup/api/domain/registration/DTOs/CreateRegistrationDTO.java
@@ -1,6 +1,29 @@
 package backend.segup.api.domain.registration.DTOs;
 
 import backend.segup.api.domain.registration.Registration;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record CreateRegistrationDTO(String cpf, String fullName, String email, String phone, Registration.DesiredServiceType desiredService, String observation) {
+public record CreateRegistrationDTO(
+
+        @NotBlank(message = "CPF é obrigatório")
+        String cpf,
+
+        @NotBlank(message = "Nome completo é obrigatório")
+        String fullName,
+
+        @NotBlank(message = "E-mail é obrigatório")
+        @Email(message = "E-mail inválido")
+        String email,
+
+        @NotBlank(message = "Telefone é obrigatório")
+        String phone,
+
+        @NotNull(message = "Serviço desejado é obrigatório")
+        Registration.DesiredServiceType desiredService,
+
+        String observation
+
+) {
 }
