d5b61d310c8ca4e688a566af9c779c1702cd1024
rotiv <vitormendesesilva1@gmail.com>
2026-05-31 04:36:04 -0300

    feat: adds update desired service dto

---
+++ api/src/main/java/backend/segup/api/domain/registration/DTOs/UpdateDesiredServiceDTO.java
@@ -0,0 +1,7 @@
+package backend.segup.api.domain.registration.DTOs;
+
+import backend.segup.api.domain.registration.Registration;
+
+public record UpdateDesiredServiceDTO(Registration.DesiredServiceType desiredService ) {
+
+}
