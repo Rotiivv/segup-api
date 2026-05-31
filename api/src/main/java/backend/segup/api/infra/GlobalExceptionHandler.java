bf071703c039e8329e0415b91aa9acc210097416
rotiv <vitormendesesilva1@gmail.com>
2026-05-31 14:21:46 -0300

    refactor: add global exception handler

---
+++ api/src/main/java/backend/segup/api/infra/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package backend.segup.api.infra;
+
+import backend.segup.api.exceptions.RegistrationNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RegistrationNotFoundException.class)
+    public ResponseEntity<String> handleNotFound(RegistrationNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+
+}
