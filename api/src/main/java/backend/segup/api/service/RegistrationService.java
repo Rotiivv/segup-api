7d9b994a12e3066465b20354912f1a89ffd39d5e
rotiv <vitormendesesilva1@gmail.com>
2026-05-31 02:43:15 -0300

    feat: adds generate protocol service and uses repository to persist the data in db

--- api/src/main/java/backend/segup/api/service/RegistrationService.java
+++ api/src/main/java/backend/segup/api/service/RegistrationService.java
@@ -2,10 +2,19 @@ package backend.segup.api.service;
 
 import backend.segup.api.domain.registration.Registration;
 import backend.segup.api.domain.registration.RegistrationRequestDTO;
+import backend.segup.api.repositories.RegistrationRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class RegistrationService {
+    private final RegistrationRepository repository;
+
+    public RegistrationService(RegistrationRepository repository) {
+        this.repository = repository;
+    }
+
     public Registration createRegistration(RegistrationRequestDTO data) {
         Registration newRegistration = new Registration();
 
@@ -16,7 +25,13 @@ public class RegistrationService {
          newRegistration.setDesiredService(data.desiredService());
          newRegistration.setStatus(data.status());
          newRegistration.setObservation(data.observation());
+         newRegistration.setProtocol(generateProtocol());
 
-         return newRegistration;
+         return repository.save(newRegistration);
+    }
+
+
+    private String generateProtocol() {
+        return "PR-" + UUID.randomUUID().toString().substring(0, 36).toUpperCase();
     }
 }
