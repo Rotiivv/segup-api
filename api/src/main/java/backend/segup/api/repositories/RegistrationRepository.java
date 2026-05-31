f04893b5db98dd8eff6e3f733d3d066161d13560
rotiv <vitormendesesilva1@gmail.com>
2026-05-31 03:15:01 -0300

    feat: adds find all registrations by cpf method in registration repository

--- api/src/main/java/backend/segup/api/repositories/RegistrationRepository.java
+++ api/src/main/java/backend/segup/api/repositories/RegistrationRepository.java
@@ -3,7 +3,9 @@ package backend.segup.api.repositories;
 import backend.segup.api.domain.registration.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RegistrationRepository extends JpaRepository<Registration, UUID> {
+    List<Registration> findAllByCpf(String cpf);
 }
