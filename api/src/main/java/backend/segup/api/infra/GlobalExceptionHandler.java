4ac9fcf2334b3fcf35c245eb7adc41642ec0af25
rotiv <vitormendesesilva1@gmail.com>
2026-05-31 14:52:33 -0300

    refactor: implement global exception handling and validation errors

--- api/src/main/java/backend/segup/api/infra/GlobalExceptionHandler.java
+++ api/src/main/java/backend/segup/api/infra/GlobalExceptionHandler.java
@@ -5,8 +5,13 @@ import backend.segup.api.exceptions.RegistrationCanceledException;
 import backend.segup.api.exceptions.RegistrationNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -34,6 +39,35 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
                 .body(exception.getMessage());
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(
+            MethodArgumentNotValidException ex
+    ) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.put(
+                                error.getField(),
+                                error.getDefaultMessage()
+                        ));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleInvalidJson(
+            HttpMessageNotReadableException exception
+    ) {
+        return ResponseEntity
+                .badRequest()
+                .body("JSON inválido ou campo com valor incompatível.");
     }
 
 }
