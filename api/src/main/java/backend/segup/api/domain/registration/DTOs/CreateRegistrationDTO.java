13fcedb90209a1d6f893a3601c51053f093540a5
rotiv <vitormendesesilva1@gmail.com>
2026-05-31 03:13:07 -0300

    refactor: rename RegistrationRequestDTO to CreateREgistrationDTO and move this file to package DTOs

---
+++ api/src/main/java/backend/segup/api/domain/registration/DTOs/CreateRegistrationDTO.java
@@ -0,0 +1,6 @@
+package backend.segup.api.domain.registration.DTOs;
+
+import backend.segup.api.domain.registration.Registration;
+
+public record CreateRegistrationDTO(String cpf, String fullName, String email, String phone, Registration.DesiredServiceType desiredService, Registration.StatusType status, String observation) {
+}
--- api/src/main/java/backend/segup/api/domain/registration/RegistrationRequestDTO.java
+++
@@ -1,4 +0,0 @@
-package backend.segup.api.domain.registration;
-
-public record RegistrationRequestDTO(String cpf, String fullName, String email, String phone, Registration.DesiredServiceType desiredService, Registration.StatusType status, String observation) {
-}
